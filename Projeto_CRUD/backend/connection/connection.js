ed0e8785d02bbdc916ae9d87c7890c773bfd9405
Wilian Krinke <krinkewilian@gmail.com>
2021-12-03 20:06:01 -0300

    teste

---
+++ Projeto_CRUD/backend/connection/connection.js
@@ -0,0 +1,13 @@
+
+const knex = require('knex')({
+  client: 'mysql2',
+  connection: {
+    host : '127.0.0.1',
+    port : 3306,
+    user : 'root',
+    password : '24875346',
+    database : 'nodecrud'
+  }
+});
+          
+module.exports = knex;
