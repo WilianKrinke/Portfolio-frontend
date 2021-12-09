2ff980cece9198ea249cf738c82cd3c5feaf4270
Wilian Krinke <krinkewilian@gmail.com>
2021-12-09 16:13:18 -0300

    testes com icones de cards

--- Projeto_CRUD/frontend/src/components/bookCard/bookCard.js
+++ Projeto_CRUD/frontend/src/components/bookCard/bookCard.js
@@ -1,12 +1,43 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useState } from 'react';
 import propTypes from 'prop-types'
-import { CardStyled, ContainerMoldCard } from './styled';
+import { CardStyled, ContainerMoldCard, ContainerToLike, IconLendItem, IconLike, IconToRight } from './styled';
+import { BsChevronRight, BsBookmarkPlus, BsHeart, BsBookmarkCheckFill,BsHeartFill } from "react-icons/bs";
 
 const Bookcard = ({bookName,category,author,resume,amount, image}) => {
+
+    const [open, setOpen] = useState(false);
+    const [isLike, setisLike] = useState(false);
+    const [isLend, setisLend] = useState(false);
+
     return (
         <>
             <CardStyled>
+                <ContainerMoldCard isOpen={open}>
+
+                </ContainerMoldCard>
+
+                <ContainerToLike isOpen={open} onMouseEnter={() => setOpen(!open)} onMouseLeave={() => setOpen(!open)}>
+                    <IconToRight isOpen={open}>
+                        <BsChevronRight />
+                    </IconToRight>
+                    <IconLike isOpen={open} title='Emprestar' onClick={() => setisLike(!isLike)}>
+                        {
+                            isLike ?
+                                <BsBookmarkCheckFill />
+                            :
+                                <BsBookmarkPlus />
+                        }
+                    </IconLike>
+                    <IconLendItem isOpen={open} title='Adicionar aos Favoritos' onClick={() => setisLend(!isLend)}>
+                        {
+                            isLend ?
+                                <BsHeartFill />
+                            :
+                                <BsHeart />
+                        }
+                    </IconLendItem>
+                </ContainerToLike>
                 
             </CardStyled>
         </>
--- Projeto_CRUD/frontend/src/components/bookCard/styled.js
+++ Projeto_CRUD/frontend/src/components/bookCard/styled.js
@@ -4,20 +4,65 @@ export const CardStyled = styled.div`
     height: 400px;
     width: 350px;
     margin: 10px;
-    padding: 10px;
     border-radius: 5px;
     border: 1px solid rgba(0, 0, 0, 0.3);
     background-color: #CCFFCB;
     transition: all .5s ease-in-out;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
     
     :hover{
-        background-color: #FE6B0A;
-        border: 1px solid #FE6B0A;
+        background-color: #C2F7BE;
     }
 `
-
 export const ContainerMoldCard = styled.div`
-    border: 1px solid;
-    width: 100%;
+    transition: all .3s ease-in-out;
+    width: ${props => props.isOpen ? '85%' : '95%'};;
     height: 100%;
+    border-radius: 5px 0px 0px 5px;
+`
+
+export const ContainerToLike = styled.div`
+    transition: all .3s ease-in-out;
+    width: ${props => props.isOpen ? '15%' : '5%'};
+    height: 100%;
+    border-radius: 0px 5px 5px 0px;
+    background-color: #CCFFCB;
+    display: flex;
+    justify-content: ${props => props.isOpen ? 'space-evenly' : 'center'};
+    align-items: center;
+    flex-direction: column;
+    border-left: 1px solid rgba(0, 0, 0, 0.3);
+    
+`
+export const IconToRight = styled.i`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    height: 80px;
+    display: ${props => props.isOpen ? 'none' : 'static'};
+    cursor: ${props => !props.isOpen && 'pointer'};
+`
+
+export const IconLike = styled.i`
+display: flex;
+justify-content: center;
+align-items: center;
+    cursor: pointer;
+    font-size: 24px;
+    height: 80px;
+    display: ${props => !props.isOpen ? 'none' : 'static'};
+`
+
+export const IconLendItem = styled.i`
+display: flex;
+justify-content: center;
+align-items: center;
+    font-size: 24px;
+    cursor: pointer;
+    height: 80px;
+    display: ${props => !props.isOpen ? 'none' : 'static'};
 `
