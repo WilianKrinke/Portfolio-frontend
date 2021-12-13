05654b5e4ba378a54d573148706dc772c597c339
Wilian Krinke <krinkewilian@gmail.com>
2021-12-13 16:28:02 -0300

    tentativa de inserção de modal

--- Projeto_CRUD/frontend/src/components/bookCard/bookCard.jsx
+++ Projeto_CRUD/frontend/src/components/bookCard/bookCard.jsx
@@ -80,8 +80,42 @@ const Bookcard = ({
     setseeMore(!seeMore);
   }
 
+  const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
+
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = '#f00';
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
     <>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      />
       <CardStyled seeMore={seeMore}>
         <ContainerMoldCard isOpen={open}>
           <FirstContainerInfo>
