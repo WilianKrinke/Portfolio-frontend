869d988a5f0a8175a14d695d587ac94d7119887b
Wilian Krinke <krinkewilian@gmail.com>
2021-12-16 15:31:08 -0300

    refatorando beckend

--- Projeto_CRUD/backend/index.js
+++ Projeto_CRUD/backend/index.js
@@ -1,12 +1,12 @@
 require('dotenv').config()
 const express = require('express');
 const authValidate = require('./utils/authMiddleware');
-const registerUser = require('./routes/registerUser');
-const userLogin = require('./routes/userLogin');
-const lendAction = require('./routes/lendAction');
-const getBooks = require('./routes/getBooks')
-const returnBook = require('./routes/returnBook');
-const addFavorite = require('./routes/addFavorite');
+const routeRegisterUser = require('./routes/registerUser');
+const routeUserLogin = require('./routes/userLogin');
+const routeLendAction = require('./routes/lendAction');
+const routeGetBooks = require('./routes/getBooks')
+const routeReturnBook = require('./routes/returnBook');
+const routeAddFavorite = require('./routes/addFavorite');
 const cors =  require('cors');
 const app = express();
 const bodyParser = require('body-parser');
@@ -15,14 +15,15 @@ app.use(cors());
 app.use(bodyParser.json())
 app.use(express.urlencoded({extended: true}))
 
-registerUser(app)
-userLogin(app)
+
+routeUserLogin(app)
+routeRegisterUser(app)
 
 app.use(authValidate)
-getBooks(app)
-lendAction(app)
-returnBook(app)
-addFavorite(app)
+routeGetBooks(app)
+routeLendAction(app)
+routeReturnBook(app)
+routeAddFavorite(app)
 
 app.listen(3001, () => {
     console.log('Listening to 3001')
