86b223631f749953bc8fec4b91722ec330ad8b20
Wilian Krinke <krinkewilian@gmail.com>
2021-12-21 15:37:18 -0300

    responsividade

--- Projeto_CRUD/frontend/src/components/modals/styleds/styled.js
+++ Projeto_CRUD/frontend/src/components/modals/styleds/styled.js
@@ -3,13 +3,20 @@ import styled from 'styled-components'
 export const DivModal = styled.div`
     height: 100%;
     width: 100%;
-    border: 1px solid rgba(0, 0, 0, 0.3);
     display: flex;
     justify-content: space-around;
     align-items: center;
     flex-direction: column;
     align-items: center;
-    padding: 30px 10px;
+    padding: 20px 20px;
+    background-color: #ffeceb;
+    border-radius: 3px;
+
+
+    @media screen and (max-width: 430px) {
+        height: 350px;
+    }
+    
    
 
     .disclaimer_lend_book{
@@ -17,20 +24,37 @@ export const DivModal = styled.div`
         letter-spacing: 2px;
         margin: 10px;
         text-align: justify;
+        
 
         p{
             font-family: 'Zen Kaku Gothic New', sans-serif;
+        }
+
+        @media screen and (max-width: 430px) {
+            p{
+                font-size: 18px;
+            }
         }
     }
 
     .container_buttons_lend_book{
         width: 100%;
+        height: 100px;
         display: flex;
-        justify-content: space-evenly;
+        justify-content: space-between;
         align-items: center;
         flex-direction: row;
         align-items: center;
+        
+
+        @media screen and (max-width: 430px) {
+            flex-direction: column;
+        }
+       
     }
+
+
+    
 `
 export const Image = styled.img`
     width: 350px;
