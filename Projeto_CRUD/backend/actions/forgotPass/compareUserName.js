ad0f9043f111eddf7aad1bfc4924d6727fdb83ad
Wilian Krinke <krinkewilian@gmail.com>
2021-12-24 00:10:15 -0300

    verificação de nome de usuario

--- Projeto_CRUD/backend/actions/forgotPass/compareUserName.js
+++ Projeto_CRUD/backend/actions/forgotPass/compareUserName.js
@@ -3,7 +3,11 @@ const knex = require("../../connection/connection");
 async function compareUserName(userName){
     const response = await knex('users').select('email').where('userName',userName)
 
-    console.log(response[0])
+    if (response[0] !== undefined) {
+        console.log('tem usuario com este nome')
+    } else {
+        console.log('Nao existe usuario')
+    }
 }
 
 module.exports = compareUserName;
