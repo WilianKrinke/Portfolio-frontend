61ba9cef5f53a27aa1bc3255cff4b0c413fa7633
Wilian Krinke <krinkewilian@gmail.com>
2021-12-29 00:30:31 -0300

    alteração de métodos de encriptação

--- Projeto_CRUD/backend/actions/forgotPass/tokenToEmail.js
+++ Projeto_CRUD/backend/actions/forgotPass/tokenToEmail.js
@@ -2,8 +2,10 @@ var cryptoJS = require("crypto-js");
 
 function tokenToEmail(response){
     const {email} = response;
-    const hash = cryptoJS.AES.encrypt(email, process.env.SALT).toString();    
-    const token = hash.replace('/', 'a')
+    const hash = cryptoJS.AES.encrypt(email, process.env.SALT).toString();
+    
+    const token = hash.split('/').join('a')
+    
     return token;    
 }
 
