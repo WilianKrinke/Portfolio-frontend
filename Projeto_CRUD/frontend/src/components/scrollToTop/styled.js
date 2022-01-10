1071c9d4af7f2c6fcc5f601d5c53c50ba48b3e86
Wilian Krinke <krinkewilian@gmail.com>
2022-01-10 00:52:58 -0300

    teste de icones

--- Projeto_CRUD/frontend/src/components/scrollToTop/styled.js
+++ Projeto_CRUD/frontend/src/components/scrollToTop/styled.js
@@ -1,11 +1,13 @@
 import styled from 'styled-components'
-import { IoMdPlay } from "react-icons/io";
+import { AiOutlineUp } from "react-icons/ai";
 
-export const ScrollArrow = styled(IoMdPlay)`
+export const ScrollArrow = styled(AiOutlineUp)`
     position: fixed;
     right: 18.52px;
     bottom: 40px;
     font-size: 48px;
-    transform: rotate(0.75turn);
+    /* transform: rotate(0.75turn); */
     cursor: pointer;
+    color: #000
 `
+
