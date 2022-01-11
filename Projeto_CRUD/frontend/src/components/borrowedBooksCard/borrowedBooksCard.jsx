a691e73b3cdb494fcce75ceea251146c6bdc78bc
Wilian Krinke <krinkewilian@gmail.com>
2022-01-10 21:22:10 -0300

    aplicação de responsividade

--- Projeto_CRUD/frontend/src/components/borrowedBooksCard/borrowedBooksCard.jsx
+++ Projeto_CRUD/frontend/src/components/borrowedBooksCard/borrowedBooksCard.jsx
@@ -1,50 +1,66 @@
 /* eslint-disable no-unused-vars */
 import React, { memo } from 'react';
 import propTypes from 'prop-types';
-import { BorrowedBookCard, ContainerActions, ContainerResume, ContainerTitle } from './styled';
+import { BorrowedBookCard, ContainerActions, ContainerResume, ContainerTitle, Icon } from './styled';
 import { format, isAfter, isBefore } from 'date-fns';
 import ReactStars from 'react-rating-stars-component';
 
 const Borrowedbookscard = ({ infoDatas }) => {
-  //console.log(infoDatas);
+  console.log(infoDatas);
 
-  const { image, bookName, rating, resume, devolutionDate } = infoDatas;
+  const { image, bookName, rating, resume, devolutionDate, lendDate } = infoDatas;
 
+  const lendDateData = new Date(lendDate);
   const dateDevolution = new Date(devolutionDate);
   const today = new Date();
+
+  const dateDevolutionFormat = format(dateDevolution, 'dd-MM-yyyy');
+  const lendDateFormat = format(lendDateData, 'dd-MM-yyyy');
 
   const isBookLate = isAfter(today, dateDevolution);
 
   console.log(isBookLate);
 
   return (
-    <BorrowedBookCard>
-      <ContainerTitle>
+    <BorrowedBookCard isBookLate={isBookLate}>
+      <ContainerTitle isBookLate={isBookLate}>
         <div className="div_img">
           <img src={image} alt="Cover Book" title="Book Cover" loading="lazy" />
         </div>
-        <div className="div_title">
-          <h3 title="Book Name">{bookName}</h3>
-        </div>
-        <div className="div_rating" title="Rating">
-          <ReactStars
-            count={5}
-            size={18}
-            value={rating}
-            isHalf={true}
-            emptyIcon={<i className="far fa-star"></i>}
-            halfIcon={<i className="fa fa-star-half-alt"></i>}
-            fullIcon={<i className="fa fa-star"></i>}
-            activeColor="#ffff00"
-          />
+
+        <div className="div_bookname_and_rating">
+          <div className="div_title">
+            <h3 title="Book Name">{bookName}</h3>
+          </div>
+          <div className="div_rating" title="Rating">
+            <ReactStars
+              count={5}
+              size={18}
+              value={rating}
+              isHalf={true}
+              emptyIcon={<i className="far fa-star"></i>}
+              halfIcon={<i className="fa fa-star-half-alt"></i>}
+              fullIcon={<i className="fa fa-star"></i>}
+              activeColor="#ffff00"
+            />
+          </div>
         </div>
       </ContainerTitle>
-      <ContainerResume>
+      <ContainerResume isBookLate={isBookLate}>
         <div className="div_resume">
           <p>{resume}</p>
         </div>
+        <div className="div_info_lend_book">
+          <p>
+            Este livro foi emprestado no dia {lendDateFormat}, com devolução para o dia {dateDevolutionFormat}, portanto
+            está
+            {isBookLate ? ' atrasado.' : ' dentro do prazo.'}
+          </p>
+        </div>
       </ContainerResume>
-      <ContainerActions />
+      <ContainerActions isBookLate={isBookLate}>
+        <Icon />
+      </ContainerActions>
     </BorrowedBookCard>
   );
 };
--- Projeto_CRUD/frontend/src/components/borrowedBooksCard/styled.js
+++ Projeto_CRUD/frontend/src/components/borrowedBooksCard/styled.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
+import { BsBookmarkCheckFill } from 'react-icons/bs';
 
 export const BorrowedBookCard = styled.article`
-    border: 1px solid;
     height: 350px;
     width: 85%;
     margin: 10px 0px;
@@ -10,35 +10,85 @@ export const BorrowedBookCard = styled.article`
     align-items: center;
     flex-flow: row nowrap;
     border-radius: 3px;
+    transition: all .3s ease-in-out;  
+    
+
+    @media screen and (max-width: 1100px) {
+        min-height: 550px;
+        flex-flow: column nowrap;
+    }
+
+    @media screen and (max-width: 570px) {
+        min-height: 600px;
+        width: 100%;
+        border-left:  ${props => props.isBookLate ? "2px solid rgba(255, 0, 0, 1)" : "2px solid rgba(0, 0, 0, 0.3)"};
+        border-bottom:  ${props => props.isBookLate ? "2px solid rgba(255, 0, 0, 1)" : "2px solid rgba(0, 0, 0, 0.3)"};
+        border-radius: 0px;
+    }
 `
 
 export const ContainerTitle = styled.div`
     padding: 10px;
     height: 100%;
     width: 25%;
-    border: 1px solid;
     display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: column;
+    border-right:  ${props => props.isBookLate ? "2px solid rgba(255, 0, 0, 1)" : "2px solid rgba(0, 0, 0, 0.3)"};    
+
+    @media screen and (max-width: 1100px) {
+        border-right: none;
+        flex-flow: row nowrap;
+        width: 100%;
+    }
+
+    @media screen and (max-width: 570px) {
+        border-right: none;
+    }
 
     .div_img{
-        width: 90%;
+        width: 180px;
         height: 75%;
         display: flex;
         justify-content: center;
         align-items: center;
-        flex-direction: column;        
+        flex-direction: column;
+  
+        
+        @media screen and (max-width: 1100px) {
+            height: 100%;
+        }
 
         img{
             width: 100%;
             height: 100%;
             cursor: pointer;
+
+            @media screen and (max-width: 570px) {
+                width: 100%;
+                height: 80%;
+            }
+        }
+    }
+
+    .div_bookname_and_rating{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+ 
+        width: 100%;
+        height: 25%; 
+
+        @media screen and (max-width: 1100px) {
+            height: 120px;
         }
     }
 
     .div_title{
-        height: 15%;
+        height: 50%;
+        width: 100%;
         padding: 5px 0px;
         display: flex;
         justify-content: center;
@@ -46,18 +96,28 @@ export const ContainerTitle = styled.div`
         flex-direction: column;
         text-align: center;
 
+        @media screen and (max-width: 1100px) {
+            height: 100px;
+        }
+
         h3{
             font-size: 16px;
         }
     }
 
     .div_rating{
-        height: 10%;
+        height: 50%;
+        width: 100%;
         padding: 5px 0px;
         display: flex;
         justify-content: center;
         align-items: center;
         flex-direction: column;
+
+
+        @media screen and (max-width: 1100px) {
+            height: 100px;
+        }
     }
 `
 
@@ -65,13 +125,39 @@ export const ContainerResume = styled.div`
     padding: 10px;
     height: 100%;
     width: 65%;
-    border: 1px solid;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    border-bottom: ${props => props.isBookLate ? "2px solid rgba(255, 0, 0, 1)" : "2px solid rgba(0, 0, 0, 0.3)"};
 
-    .div_resume{
+    @media screen and (max-width: 1100px) {
+        width: 100%;
+        border-bottom: none;
+    }
+
+    .div_resume{   
+        padding: 10px;         
         
-
         p{
             font-size: 16px;
+        }
+
+        @media screen and (max-width: 570px) {
+             p{
+                 font-size: 16px;
+             }
+        }
+    }
+
+    .div_info_lend_book{ 
+        padding: 10px;
+        width: 100% ;        
+        
+        @media screen and (max-width: 570px) {
+             p{
+                 font-size: 16px;
+             }
         }
     }
 `
@@ -79,6 +165,21 @@ export const ContainerResume = styled.div`
 export const ContainerActions = styled.div`
     padding: 10px;
     height: 100%;
-    width: 10%;
-    border: 1px solid;
+    width: 15%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    border-bottom: ${props => props.isBookLate ? "2px solid rgba(255, 0, 0, 1)" : "2px solid rgba(0, 0, 0, 0.3)"}; 
+
+    @media screen and (max-width: 1100px) {
+        height: 20%;
+        width: 100%;
+        border-bottom: none;
+    }
+`
+
+export const Icon = styled(BsBookmarkCheckFill)`
+    font-size: 38px;
+    cursor: pointer;
 `
