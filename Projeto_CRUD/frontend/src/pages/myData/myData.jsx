dd4d3fb8f8f58280d9cef4168bb18e961eb9376a
Wilian Krinke <krinkewilian@gmail.com>
2022-01-19 21:22:42 -0300

    inserção de loading em página my datas

--- Projeto_CRUD/frontend/src/pages/myData/myData.jsx
+++ Projeto_CRUD/frontend/src/pages/myData/myData.jsx
@@ -1,26 +1,34 @@
 /* eslint-disable no-unused-vars */
-import React, { useEffect, memo } from 'react';
+import React, { useEffect, memo, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import LetterFooter from '../../components/letterFooter/letterFooter';
 import Letterheader from '../../components/letterHeader/letterHeader';
+import Loading from '../../components/loading/Loading';
 import Menu from '../../components/menu/Menu';
-import { MainStyled } from '../../primeComponents';
+import { DivLoading, MainStyled } from '../../primeComponents';
 import getMyDatas from '../../utils/getMyDatas/getMyDatas';
 import tokenTimeOut from '../../utils/tokenTimeOut/tokenTimeOut';
 
 const Mydata = () => {
   const navigate = useNavigate();
 
+  const [userNameState, setuserNameState] = useState('');
+  const [loadingState, setloadingState] = useState(true);
+
   useEffect(() => {
     (async () => {
       try {
         const response = await getMyDatas();
+        console.log(response);
 
         if (response === false) {
           tokenTimeOut(navigate);
         } else {
-          //
+          const { userName, userDatas } = response;
+
+          setuserNameState(userName);
+          setloadingState(false);
         }
       } catch (e) {
         const error = new Error(e);
@@ -31,10 +39,18 @@ const Mydata = () => {
 
   return (
     <>
-      <Menu />
-      <Letterheader phrase="My Datas" />
-      <MainStyled></MainStyled>
-      <LetterFooter />
+      {loadingState ? (
+        <DivLoading>
+          <Loading />
+        </DivLoading>
+      ) : (
+        <>
+          <Menu user={userNameState} />
+          <Letterheader phrase="My Datas" />
+          <MainStyled></MainStyled>
+          <LetterFooter />
+        </>
+      )}
     </>
   );
 };
