d82051366f28f5913423e5fa68e26ba61cdb61ae
Wilian Krinke <krinkewilian@gmail.com>
2022-01-19 21:00:35 -0300

    inserindo colunas em entidade users

--- Projeto_CRUD/backend/actions/getDatasActions/getDatas.js
+++ Projeto_CRUD/backend/actions/getDatasActions/getDatas.js
@@ -1,8 +1,11 @@
 const knex = require('../../connection/connection')
 
 async function getDatas(idUser){
-    const response = await knex('users').where('idUser', idUser);
-    //fazer seleção de dados
+    const response = await knex('users').select('email', 'data_nascimento','endereco_logradouro', 'endereco_numero', 'endereco_bairro', 'endereco_cidade', 'telefone_fixo', 'telefone_celular', 'primeiro_nome', 'segundo_nome').where('idUser', idUser);
+
+    console.log(response)
+
+
 
     if (response[0] !== undefined) {
        return response
