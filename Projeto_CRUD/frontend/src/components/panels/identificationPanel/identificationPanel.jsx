c43d01f94115936880a59567159fe90bf59d491e
Wilian Krinke <krinkewilian@gmail.com>
2022-01-22 17:48:20 -0300

    inserção de funções em forms

--- Projeto_CRUD/frontend/src/components/panels/identificationPanel/identificationPanel.jsx
+++ Projeto_CRUD/frontend/src/components/panels/identificationPanel/identificationPanel.jsx
@@ -7,11 +7,31 @@ import { ButtonUpDate } from '../../Buttons';
 const Identificationpanel = ({ objectUserIdentification }) => {
   const { userName, primeiro_nome, segundo_nome, data_nascimento } = objectUserIdentification;
 
+  function handleUserName(e) {
+    e.preventDefault();
+    console.log('Chamou handle UserName');
+  }
+
+  function handleFirstName(e) {
+    e.preventDefault();
+    console.log('Chamou handle first Name');
+  }
+
+  function handleSecondName(e) {
+    e.preventDefault();
+    console.log('Chamou handle second name');
+  }
+
+  function handleBirth(e) {
+    e.preventDefault();
+    console.log('Chamou handle birth');
+  }
+
   return (
     <>
       <MainContainer>
         <ContainerInput>
-          <FormStyled component="form" noValidate autoComplete="off">
+          <FormStyled component="form" noValidate autoComplete="off" onSubmit={(e) => handleUserName(e)}>
             <TextField
               id="outlined-basic"
               label={userName === null ? 'Insert User Name' : userName}
@@ -21,7 +41,7 @@ const Identificationpanel = ({ objectUserIdentification }) => {
             <ButtonUpDate>Update</ButtonUpDate>
           </FormStyled>
 
-          <FormStyled component="form" noValidate autoComplete="off">
+          <FormStyled component="form" noValidate autoComplete="off" onSubmit={(e) => handleFirstName(e)}>
             <TextField
               id="outlined-basic"
               label={primeiro_nome === null ? 'Insert First Name' : primeiro_nome}
@@ -31,7 +51,7 @@ const Identificationpanel = ({ objectUserIdentification }) => {
             <ButtonUpDate>Update</ButtonUpDate>
           </FormStyled>
 
-          <FormStyled component="form" noValidate autoComplete="off">
+          <FormStyled component="form" noValidate autoComplete="off" onSubmit={(e) => handleSecondName(e)}>
             <TextField
               id="outlined-basic"
               label={segundo_nome === null ? 'Insert Second Name' : segundo_nome}
@@ -41,7 +61,7 @@ const Identificationpanel = ({ objectUserIdentification }) => {
             <ButtonUpDate>Update</ButtonUpDate>
           </FormStyled>
 
-          <FormStyled component="form" noValidate autoComplete="off">
+          <FormStyled component="form" noValidate autoComplete="off" onSubmit={(e) => handleBirth(e)}>
             <TextField
               id="outlined-basic"
               label={data_nascimento === null ? 'Insert Year Of Birth' : data_nascimento}
