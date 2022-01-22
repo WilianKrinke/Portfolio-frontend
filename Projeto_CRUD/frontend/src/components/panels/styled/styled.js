6ed4b64a0911ec87f02caab690e531f6ab95f814
Wilian Krinke <krinkewilian@gmail.com>
2022-01-21 22:17:43 -0300

    visualização de dados do usuario

--- Projeto_CRUD/frontend/src/components/panels/styled/styled.js
+++ Projeto_CRUD/frontend/src/components/panels/styled/styled.js
@@ -2,11 +2,11 @@ import styled from 'styled-components'
 import Box from '@mui/material/Box';
 
 export const MainContainer = styled.div`
-    height: 320px;
+    min-height: 320px;
     width: 800px;
     display: flex;
     align-items: center;
-    justify-content: center;
+    justify-content: space-around;
     flex-direction: row;
     text-align: center;    
 
@@ -21,11 +21,11 @@ export const FormStyled = styled(Box)`
     align-items: center;
     justify-content: space-evenly;
     flex-direction: column;
-    height: 100%;
-    width: 100%;
+    height: 310px;
+    width: 100%;   
 
     @media screen and (max-width: 600px) {
-        height: 50%
+        height: 310px;
     }
 `
 export const ContainerStyledShowDatas = styled.div`
@@ -33,9 +33,35 @@ export const ContainerStyledShowDatas = styled.div`
     align-items: center;
     justify-content: space-evenly;
     flex-direction: column;
-    height: 100%;
+    height: 320px;
     width: 100%;
-    border: 1px solid;
+
+    .div_name, .div_userName, .div_birth,  .div_adress{
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        flex-direction: row;
+        width: 100%;
+        padding: 0px 10px;
+    }
+
+    .div_adress_street, .div_adress_district, .div_adress_number, .div_adress_city{
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        flex-direction: row;
+        width: 100%;
+        padding: 0px 10px;
+    }
+
+    .div_email, .div_cell_phone, .div_landline{
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        flex-direction: row;
+        width: 100%;
+        padding: 0px 10px;
+    }
 
     @media screen and (max-width: 600px) {
         width: 340px;
