56bb30a3809fb942bfe92a6f63ec8cd694d9dbf8
Wilian Krinke <krinkewilian@gmail.com>
2022-01-25 15:07:10 -0300

    Inserindo funções de controle no frontend

--- Projeto_CRUD/frontend/src/components/panels/adressPanel/adressPanel.jsx
+++ Projeto_CRUD/frontend/src/components/panels/adressPanel/adressPanel.jsx
@@ -1,73 +1,171 @@
 /* eslint-disable no-unused-vars */
-import React, { memo } from 'react';
+import React, { memo, useState } from 'react';
 import propTypes from 'prop-types';
 import { MainContainer, ContainerInput, FormStyled } from '../styled/styled';
 import { ButtonUpDate } from '../../Buttons';
 import TextField from '@mui/material/TextField';
+import upDateData from '../../../utils/upDateData/upDateData';
+import tokenTimeOut from '../../../utils/tokenTimeOut/tokenTimeOut';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 const Adresspanel = ({ objectUserAdress }) => {
+  const navigate = useNavigate();
   const { endereco_logradouro, endereco_bairro, endereco_numero, endereco_cidade } = objectUserAdress;
 
-  function handleAdressStreet(e) {
-    e.preventDefault();
-    console.log('Chamou a func');
+  const [endereco_LogradouroState, setEndereco_LogradouroState] = useState('');
+  const [endereco_BairroState, setEndereco_BairroState] = useState('');
+  const [endereco_NumeroState, setEndereco_NumeroState] = useState('');
+  const [endereco_CidadeState, setEndereco_CidadeState] = useState('');
+
+  async function handleAdressStreet(e) {
+    try {
+      e.preventDefault();
+      const objectData = {
+        data: endereco_logradouro,
+        option: 5,
+      };
+
+      const response = await upDateData(objectData);
+
+      if (response === false) {
+        tokenTimeOut(navigate);
+      } else {
+        toast.success('UpDated Data');
+      }
+    } catch (error) {
+      if (error.message === 'Contains Inappropriate Characters') {
+        toast.error('Contains Inappropriate Characters');
+      }
+
+      console.log(error);
+      //pagina de erros
+    }
   }
 
-  function handleAdressNeighborhood(e) {
-    e.preventDefault();
-    console.log('Chamou a func');
+  async function handleAdressNeighborhood(e) {
+    try {
+      e.preventDefault();
+      const objectData = {
+        data: endereco_bairro,
+        option: 6,
+      };
+
+      const response = await upDateData(objectData);
+
+      if (response === false) {
+        tokenTimeOut(navigate);
+      } else {
+        toast.success('UpDated Data');
+      }
+    } catch (error) {
+      if (error.message === 'Contains Inappropriate Characters') {
+        toast.error('Contains Inappropriate Characters');
+      }
+
+      console.log(error);
+      //pagina de erros
+    }
   }
 
-  function handleAdressNumber(e) {
-    e.preventDefault();
-    console.log('Chamou a func');
+  async function handleAdressNumber(e) {
+    try {
+      e.preventDefault();
+      const objectData = {
+        data: endereco_numero,
+        option: 7,
+      };
+
+      const response = await upDateData(objectData);
+
+      if (response === false) {
+        tokenTimeOut(navigate);
+      } else {
+        toast.success('UpDated Data');
+      }
+    } catch (error) {
+      if (error.message === 'Contains Inappropriate Characters') {
+        toast.error('Contains Inappropriate Characters');
+      }
+
+      console.log(error);
+      //pagina de erros
+    }
   }
 
-  function handleCityAdress(e) {
-    e.preventDefault();
-    console.log('Chamou a func');
+  async function handleCityAdress(e) {
+    try {
+      e.preventDefault();
+      const objectData = {
+        data: endereco_cidade,
+        option: 8,
+      };
+
+      const response = await upDateData(objectData);
+
+      if (response === false) {
+        tokenTimeOut(navigate);
+      } else {
+        toast.success('UpDated Data');
+      }
+    } catch (error) {
+      if (error.message === 'Contains Inappropriate Characters') {
+        toast.error('Contains Inappropriate Characters');
+      }
+
+      console.log(error);
+      //pagina de erros
+    }
   }
 
   return (
     <>
       <MainContainer>
         <ContainerInput>
-          <FormStyled component="form" noValidate autoComplete="off" onSubmit={(e) => handleAdressStreet(e)}>
+          <FormStyled component="form" autoComplete="off" onSubmit={(e) => handleAdressStreet(e)}>
             <TextField
               id="outlined-basic"
               label={endereco_logradouro === null ? 'Insert Address Street' : endereco_logradouro}
               variant="outlined"
               title="Update Your Address Street"
+              required
+              onChange={(e) => setEndereco_LogradouroState(e.target.value)}
             />
             <ButtonUpDate>Update</ButtonUpDate>
           </FormStyled>
 
-          <FormStyled component="form" noValidate autoComplete="off" onSubmit={(e) => handleAdressNeighborhood(e)}>
+          <FormStyled component="form" autoComplete="off" onSubmit={(e) => handleAdressNeighborhood(e)}>
             <TextField
               id="outlined-basic"
               label={endereco_bairro === null ? 'Insert Address Neighborhood' : endereco_bairro}
               variant="outlined"
               title="Update Your Address Neighborhood"
+              required
+              onChange={(e) => setEndereco_BairroState(e.target.value)}
             />
             <ButtonUpDate>Update</ButtonUpDate>
           </FormStyled>
 
-          <FormStyled component="form" noValidate autoComplete="off" onSubmit={(e) => handleAdressNumber(e)}>
+          <FormStyled component="form" autoComplete="off" onSubmit={(e) => handleAdressNumber(e)}>
             <TextField
               id="outlined-basic"
               label={endereco_numero === null ? 'Insert Address Number' : endereco_numero}
               variant="outlined"
               title="Update Your Address Number"
+              required
+              onChange={(e) => setEndereco_NumeroState(e.target.value)}
             />
             <ButtonUpDate>Update</ButtonUpDate>
           </FormStyled>
 
-          <FormStyled component="form" noValidate autoComplete="off" onSubmit={(e) => handleCityAdress(e)}>
+          <FormStyled component="form" autoComplete="off" onSubmit={(e) => handleCityAdress(e)}>
             <TextField
               id="outlined-basic"
               label={endereco_cidade === null ? 'Insert City ​​Address' : endereco_cidade}
               variant="outlined"
               title="Update Your City ​​Address"
+              required
+              onChange={(e) => setEndereco_CidadeState(e.target.value)}
             />
             <ButtonUpDate>Update</ButtonUpDate>
           </FormStyled>
