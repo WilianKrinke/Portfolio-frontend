9050cda86fa58885a940a9cdcbb142c8d79a15e6
Wilian Krinke <krinkewilian@gmail.com>
2022-01-26 21:18:04 -0300

    alterações em css

--- Projeto_CRUD/frontend/src/App.jsx
+++ Projeto_CRUD/frontend/src/App.jsx
@@ -1,16 +1,16 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import Routes from './routes/index.js';
 import store from './store/store';
 import { Provider } from 'react-redux';
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, Slide } from 'react-toastify';
 
 function App() {
   return (
     <Provider store={store}>
       <Routes />
-
       <ToastContainer
-        position="bottom-right"
+        position="top-right"
         autoClose={3000}
         hideProgressBar={false}
         newestOnTop
@@ -19,6 +19,7 @@ function App() {
         draggable
         pauseOnHover={false}
         width={500}
+        transition={Slide}
       />
     </Provider>
   );
