3512264b7be7a32e19d3aa5a2ece93c576d33b2f
Wilian Krinke <krinkewilian@gmail.com>
2022-01-29 00:01:21 -0300

    formando página de contato

--- Projeto_CRUD/frontend/src/pages/contact/contactPage.jsx
+++ Projeto_CRUD/frontend/src/pages/contact/contactPage.jsx
@@ -9,6 +9,7 @@ import { DivLoading, MainStyled } from '../../primeComponents';
 import acessUser from '../../utils/accessUser/acessUser';
 import tokenTimeOut from '../../utils/tokenTimeOut/tokenTimeOut';
 import { SectionContainer } from '../myBorrowedBooks/styled';
+import { ArticleContainer, DivPhysicalAddress, FormStyled } from './styled';
 
 const ContactPage = () => {
   const [loadingState, setloadingState] = useState(true);
@@ -47,7 +48,31 @@ const ContactPage = () => {
           <Menu user={userNameState} />
           <Letterheader phrase="Contact Us" />
           <MainStyled>
-            <SectionContainer></SectionContainer>
+            <SectionContainer>
+              <ArticleContainer>
+                <DivPhysicalAddress>
+                  <div className="div_title">
+                    <h2>Contact</h2>
+                  </div>
+                  <div className="div_adress">
+                    <address>Lorem Ipsum Street, Lorem Ipsum District, Lorem Ipsum City</address>
+                    <p>+99 99 99999-9993</p>
+                    <p>Lorem-E-mail@email.com</p>
+                    <iframe
+                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d58879.017085215884!2d-46.933618992296786!3d-22.730524619711403!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c8dea2fead7493%3A0xaae359ef9cc88d5f!2sPedreira%2C%20SP%2C%2013920-000!5e0!3m2!1spt-BR!2sbr!4v1643423901136!5m2!1spt-BR!2sbr"
+                      width="600"
+                      height="200"
+                      style={{ border: '1px solid rgba(0, 0, 0, 0.2)', borderRadius: '2px' }}
+                      loading="lazy"
+                    ></iframe>
+                  </div>
+                </DivPhysicalAddress>
+                <FormStyled>
+                  <div className="div_from"></div>
+                  <div className="div_"></div>
+                </FormStyled>
+              </ArticleContainer>
+            </SectionContainer>
           </MainStyled>
           <LetterFooter />
         </>
---
+++ Projeto_CRUD/frontend/src/pages/contact/styled.js
@@ -0,0 +1,53 @@
+import styled from 'styled-components'
+
+export const ArticleContainer = styled.article`    
+    height: 400px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    flex-direction: row;
+`
+
+export const DivPhysicalAddress = styled.div`
+    height: 400px;
+    width: 50%;
+    padding: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    flex-direction: column;
+    border-right: 1px solid rgba(0, 0, 0, 0.3);
+
+    .div_title{
+        width: 100%;
+        text-align: center;
+        
+
+        h2{
+            letter-spacing: 3px;
+        }
+    }
+
+    .div_adress{
+        width: 100%;
+        height: 300px;
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+        flex-direction: column;
+    }
+`
+
+export const FormStyled = styled.form`
+    border-left: 1px solid rgba(0, 0, 0, 0.3);
+    border: 1px solid;
+    height: 400px;
+    width: 50%;
+    padding: 10px;
+
+    .div_from{
+        border: 1px solid;
+        width: 100%;
+    }
+`
