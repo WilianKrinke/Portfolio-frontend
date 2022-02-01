42a0d7e4abd60bc61948a498dbc9bcd45b993b50
Wilian Krinke <krinkewilian@gmail.com>
2022-02-01 16:49:19 -0300

    alterações em css e componentização

--- Projeto_CRUD/frontend/src/components/ajustBookList/styled.js
+++ Projeto_CRUD/frontend/src/components/ajustBookList/styled.js
@@ -38,46 +38,13 @@ export const DivSelectCategory = styled.div`
     background-color: transparent;
     transition: all .7s ease-in-out;
     padding: 10px;
-    border: 1px solid ${props => props.darkMode ? "#F9FFF9" : "rgba(0, 0, 0, 0.1)"};
-
-    label {
-        position: relative;
-        bottom: 2px
-    }
-
-    select {
-        outline: none;
-        border: none;
-        background-color: transparent; 
-    }  
+    border: 1px solid ${props => props.darkMode ? "rgba(84, 84, 84, 0.4)" : "rgba(0, 0, 0, 0.1)"};  
 
     @media screen and (max-width: 808px) {
-        label{
-            font-size: 20px;
-            position: relative;
-            bottom: 3px
-        }
-
-        select {
-            font-size: 18px;
-            font-weight: 200;
-        }
-
-        width: 100%;
+        width: 95%;
     }
 
     @media screen and (max-width: 440px) {
-        label{
-            font-size: 20px;
-            position: relative;
-            bottom: 3px
-        }
-
-        select {
-            font-size: 18px;
-            font-weight: 200;
-        }
-
         width: 100%;
     }
 `
@@ -92,48 +59,14 @@ export const DivSelectQnt = styled.div`
     padding: 10px;
     background-color: transparent;
     border-radius: 5px;
-    border: 1px solid ${props => props.darkMode ? "#F9FFF9" : "rgba(0, 0, 0, 0.1)"};
+    border: 1px solid ${props => props.darkMode ? "rgba(84, 84, 84, 0.4)" : "rgba(0, 0, 0, 0.1)"};
     transition: all .7s ease-in-out;
 
-    label {
-        position: relative;
-        bottom: 2px
-    }
-
-    select {
-        outline: none;
-        border: none;
-        background-color: transparent;
-    } 
-
-    @media screen and (max-width: 808px) {
-        label{
-            font-size: 20px;
-            position: relative;
-            bottom: 3px
-        }
-
-        select {
-            font-size: 18px;
-            font-weight: 200;
-        }
-        
-        width: 100%;
+    @media screen and (max-width: 808px) {        
+        width: 95%;
     }
     
-    @media screen and (max-width: 440px) {
-
-        label{
-            font-size: 20px;
-            position: relative;
-            bottom: 3px
-        }
-
-        select {
-            font-size: 18px;
-            font-weight: 200;
-        }
-        
+    @media screen and (max-width: 440px) {        
         width: 100%;    
     }
 `
@@ -147,16 +80,11 @@ export const DivChangePage = styled.div`
     position: relative;
     bottom: 2px;
 
-    select{
-        background: #F9FFF9;
-    }
-
     @media screen and (max-width: 808px) {
-        width: 100%;   
+        width: 80%;   
     }
 
-    @media screen and (max-width: 440px) {
-        
+    @media screen and (max-width: 440px) {        
         width: 100%;    
     }
 `
@@ -171,12 +99,6 @@ export const IconBack = styled.i`
 `
 
 export const InfoPagination = styled.div`
-    span{
-        font-size: 35px;
-        letter-spacing: 5px;
-        position: relative;
-        left: 3px
-    }
 `
 
 export const IconForward = styled.i`
@@ -195,5 +117,42 @@ export const Span = styled.span`
     position: relative;
     left: 3px;
     color: ${props => props.darkMode ? "#F9FFF9" : "#0a0a0a"};      
+    transition: all .7s ease-in-out;
+`
+
+export const Label = styled.label`
+    position: relative;
+    bottom: 2px;
+    color: ${props => props.darkMode ? "#F9FFF9" : "#0a0a0a"};
+    transition: all .7s ease-in-out;
+
+    @media screen and (max-width: 808px) {
+        font-size: 20px;
+        position: relative;
+        bottom: 3px;
+    }
+
+    @media screen and (max-width: 440px) {        
+        font-size: 20px;
+        position: relative;
+        bottom: 3px;        
+    }
+`
+
+export const Select = styled.select`
+    outline: none;
+    border: none;
+    background-color: transparent; 
+    color: ${props => props.darkMode ? "#F9FFF9" : "#0a0a0a"};
+
+    @media screen and (max-width: 808px) {
+        font-size: 18px;
+        font-weight: 200;
+    }
+`
+
+export const Option = styled.option`
+    background-color: ${props => props.darkMode ? "#0e1324" : "#F9FFF9"};
+    color: ${props => props.darkMode ? "#F9FFF9" : "#0a0a0a"};   
     transition: all .7s ease-in-out;
 `
