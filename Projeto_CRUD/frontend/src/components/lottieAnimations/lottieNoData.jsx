99546b77f966c40aa22d4901a7efcd732ec9a7c7
Wilian Krinke <krinkewilian@gmail.com>
2022-02-05 17:01:32 -0300

    modo escuro em lottie files

--- Projeto_CRUD/frontend/src/components/lottieAnimations/lottieNoData.jsx
+++ Projeto_CRUD/frontend/src/components/lottieAnimations/lottieNoData.jsx
@@ -1,10 +1,14 @@
 import lottie from 'lottie-web';
 import propTypes from 'prop-types';
 import React, { memo, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import noresults from '../../assets/no-results.json';
+import { Span } from './styled';
 import './styled.css';
 
 const Lottienodata = ({ word }) => {
+    const darkMode = useSelector((state) => state.toggleDarkModeReducer.darkMode);
+
     useEffect(() => {
         lottie.loadAnimation({
             container: document.querySelector('#NoData'),
@@ -20,8 +24,12 @@ const Lottienodata = ({ word }) => {
             <section className="section" title="No Data to Display">
                 <div id="NoData" style={{ width: '100%', height: 350 }} />
                 <div className="div_words">
-                    <span className="word1">No</span>
-                    <span className="word2">{word}</span>
+                    <Span className="word1" $darkmode={darkMode}>
+                        No
+                    </Span>
+                    <Span className="word2" $darkmode={darkMode}>
+                        {word}
+                    </Span>
                 </div>
             </section>
         </>
---
+++ Projeto_CRUD/frontend/src/components/lottieAnimations/styled.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+export const Span = styled.span`
+    color: ${props => props.$darkmode ? '#fff' : '#0a0a0a'};
+`
