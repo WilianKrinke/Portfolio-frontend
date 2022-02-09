e50f17d29f5e6f30bd7c3f95d25635c1cc1c098f
Wilian Krinke <krinkewilian@gmail.com>
2022-02-09 01:14:23 -0300

    insertion modal blocked

---
+++ Projeto_CRUD/frontend/src/components/modals/modalBlocked.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Modal from 'react-modal';
+
+const Modalblocked = () => {
+    const customStyles = {
+        content: {
+            position: 'fixed',
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            textAlign: 'center',
+            border: 'none',
+            backgroundColor: 'transparent',
+        },
+        overlay: {
+            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+            boxShadow: '0 4px 30px rgba(0, 0, 0, 0.1)',
+            backdropFilter: 'blur(5.9px)',
+            position: 'fixed',
+        },
+    };
+
+    return (
+        <>
+            <Modal ariaHideApp={false} style={customStyles} contentLabel="Confirmed Modal"></Modal>
+        </>
+    );
+};
+
+export default Modalblocked;
