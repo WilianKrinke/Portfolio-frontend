9f9487e4a8d71d5cd60d5fc9077666946e4caf80
Wilian Krinke <krinkewilian@gmail.com>
2022-02-13 00:52:52 -0300

    retirando log e aguardando teste de bloqueio

--- Projeto_CRUD/frontend/src/utils/isUserBlocked/isUserBlocked.js
+++ Projeto_CRUD/frontend/src/utils/isUserBlocked/isUserBlocked.js
@@ -4,7 +4,6 @@ import baseUrl from "../baseUrl";
 export default async function isUserBlocked(){
     preAuth()
     const response = await baseUrl.get('/user-blocked')
-    console.log(response.data)
     const {data} = response;
     return data;    
 }
