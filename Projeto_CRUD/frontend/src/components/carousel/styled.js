4ca4c1a93e352e83d89a7173ea37b752eb761f46
Wilian Krinke <krinkewilian@gmail.com>
2022-02-19 01:53:18 -0300

    verificando sobescrita de css

--- Projeto_CRUD/frontend/src/components/carousel/styled.js
+++ Projeto_CRUD/frontend/src/components/carousel/styled.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export const DivCarousel = styled.div`
     border: 1px solid #000;
@@ -8,10 +8,35 @@ export const DivCarousel = styled.div`
     flex-direction: row;
     height: 400px;
     width: 100vw;
+
+    .carouselProvider{
+        border: 1px solid #000;
+        height: 400px;
+        width: 100vw;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        flex-direction: row;
+    }
+
+    .slider{
+        border: 1px solid #000;
+        height: 400px;
+        width: 90vw;
+        
+    }
+
+    .btn_left{
+        /*  */
+    }
+
+    .btn_right{
+        /*  */
+    }
 `
 
 export const DivItem = styled.div`
-    border: 1px solid #000;
+border: 1px solid #000;
     display: flex;
     justify-content: space-around;
     align-items: center;
--- Projeto_CRUD/frontend/src/components/carousel/tutorialCarousel.jsx
+++ Projeto_CRUD/frontend/src/components/carousel/tutorialCarousel.jsx
@@ -1,15 +1,21 @@
 import { ButtonBack, ButtonNext, CarouselProvider, Slide, Slider } from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
 import React from 'react';
-import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
 import { DivCarousel, DivItem } from './styled';
 
 const Tutorialcarousel = () => {
     return (
         <>
             <DivCarousel>
-                <CarouselProvider totalSlides={3} touchEnabled>
-                    <Slider style={{ height: '400px', width: '90vw', position: 'relative', top: '11px' }}>
+                <CarouselProvider
+                    naturalSlideWidth={100}
+                    naturalSlideHeight={100}
+                    totalSlides={3}
+                    touchEnabled
+                    className="carouselProvider"
+                >
+                    <ButtonBack className="btn_left">Back</ButtonBack>
+                    <Slider className="slider">
                         <Slide index={0}>
                             <DivItem>
                                 <p>Item 1</p>
@@ -26,14 +32,7 @@ const Tutorialcarousel = () => {
                             </DivItem>
                         </Slide>
                     </Slider>
-                    <div>
-                        <ButtonBack style={{ border: 'none', backgroundColor: 'transparent' }}>
-                            {BsChevronCompactLeft}
-                        </ButtonBack>
-                        <ButtonNext style={{ border: 'none', backgroundColor: 'transparent' }}>
-                            {BsChevronCompactRight}
-                        </ButtonNext>
-                    </div>
+                    <ButtonNext className="btn_right">Next</ButtonNext>
                 </CarouselProvider>
             </DivCarousel>
         </>
