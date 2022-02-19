32f10df6f05424b1310f1205ef7be0c70d38e11d
Wilian Krinke <krinkewilian@gmail.com>
2022-02-19 00:39:06 -0300

    inserindo carousel

---
+++ Projeto_CRUD/frontend/src/components/carousel/styled.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const DivCarousel = styled.div`
+    border: 1px solid #000;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    flex-direction: row;
+    height: 400px;
+    width: 100vw;
+`
+
+export const DivItem = styled.div`
+    border: 1px solid #000;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    flex-direction: column;
+    height: 400px;
+`
--- Projeto_CRUD/frontend/src/components/carousel/tutorialCarousel.jsx
+++ Projeto_CRUD/frontend/src/components/carousel/tutorialCarousel.jsx
@@ -1,7 +1,38 @@
+import { ButtonBack, ButtonNext, CarouselProvider, Slide, Slider } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
 import React from 'react';
+import { DivCarousel, DivItem } from './styled';
 
 const Tutorialcarousel = () => {
-    return <>{/*  */}</>;
+    return (
+        <>
+            <DivCarousel>
+                <CarouselProvider totalSlides={3} touchEnabled>
+                    <Slider style={{ height: '400px', width: '90vw', position: 'relative', top: '11px' }}>
+                        <Slide index={0}>
+                            <DivItem>
+                                <p>Item 1</p>
+                            </DivItem>
+                        </Slide>
+                        <Slide index={1}>
+                            <DivItem>
+                                <p>Item 2</p>
+                            </DivItem>
+                        </Slide>
+                        <Slide index={2}>
+                            <DivItem>
+                                <p>Item 3</p>
+                            </DivItem>
+                        </Slide>
+                    </Slider>
+                    <div>
+                        <ButtonBack>Back</ButtonBack>
+                        <ButtonNext>Next</ButtonNext>
+                    </div>
+                </CarouselProvider>
+            </DivCarousel>
+        </>
+    );
 };
 
 export default Tutorialcarousel;
