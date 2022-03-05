90b3c553bb8b0f23175290ea2a477bae2420cb2e
Wilian Krinke <krinkewilian@gmail.com>
2022-03-05 01:06:37 -0300

    traduções

--- Projeto_CRUD/frontend/src/i18n/messages/es-ES.js
+++ Projeto_CRUD/frontend/src/i18n/messages/es-ES.js
@@ -8,6 +8,8 @@ export default {
         'homeTitle': 'Proyecto de Biblioteca',
         'firstParagraph': 'Inicialmente el proyecto de la biblioteca se desarrolló como un simple Crear, Leer, Actualizar, Eliminar, para mi portafolio, pero a medida que avanzaban las fases de desarrollo, me di cuenta que podía hacer un sistema de préstamo y devolución de libros, por supuesto, como un trabajo de desarrollo web.',
         'secondParagraph': 'El Proyecto Biblioteca está inspirado en un C.R.U.D. da la universidad, especificamente en la materia de Fundamentos de Desarrollo Web, donde se aplicaron solo hmtl, css y php, por lo que puedo decir que es un "remake" del trabajo de la facultad, que también estará en mi carpeta. Finalmente, espero que les guste el proyecto, los animo a evaluar el código que pondré disponible {path} y si quieren pueden darle me gusta y compartir, seguirme en github y recomendar habilidades en linkedin.',
+        'tutorialexternal':'¿Alguna pregunta? {tutorialExternal}',
+        'linktutorialExternal': 'Tutorial',
         'signup':'Inscribirse',
         'signin': 'Iniciar Sesión',
         'forgotyourpassword':'¿Olvidaste tu contraseña?',
