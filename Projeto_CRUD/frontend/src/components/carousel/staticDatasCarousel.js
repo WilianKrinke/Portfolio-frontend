9a38cf6c99807cc4a6db6d302135abba84322cf6
Wilian Krinke <krinkewilian@gmail.com>
2022-03-08 01:36:22 -0300

    refatorando tutorial

--- Projeto_CRUD/frontend/src/components/carousel/staticDatasCarousel.js
+++ Projeto_CRUD/frontend/src/components/carousel/staticDatasCarousel.js
@@ -9,45 +9,42 @@ const arrayObjectPhotos = [
     {
         id: 2,
         photo: Foto2,
-        description:
-            "Log in with your username and password. If you don't have an account, click sign up. If you forgot your password, click the recovery link.",
+        description: "If you don't have an account, you can quickly register with your email, just create a secure password.",
     },
     {
         id: 3,
         photo: Foto3,
-        description: 'If you do not have a registration, click on sign up, fill in the form and click on register.',
+        description: 'If you forgot your password, click on "forgot your password?". You will receive an email with a password reset instruction.',
     },
     {
         id: 4,
         photo: Foto4,
-        description:
-            'If you forgot your password, click on "forgot your password?". You will receive an email with a password reset instruction.',
+        description: 'On every page you will have the menu, with it you can change light/dark mode, browse the site, and exit. At the end it is indicated whether your data is up to date or not.',
     },
     {
         id: 5,
         photo: Foto5,
-        description: 'To borrow a book, click on the unmarked flag. Marked flags mean you lent the book.',
+        description: 'On the book loan page, you can adjust the display of book quantity, theme and page.',
     },
     {
         id: 6,
         photo: Foto6,
-        description: 'Before confirming the book loan, carefully read the instructions on delays, then click next.',
+        description: 'Click borrow book to borrow, click heart to add to favorites, click on the photo to view it larger, click on the rating to rate it, click on the arrow to see more of the summary.',
     },
     {
         id: 7,
         photo: Foto7,
-        description: 'Confirm or cancel the loan.',
+        description: 'Before borrowing the book, pay attention to the borrowing rules. You can change the frames by clicking on the words "previous" and "next".',
     },
     {
         id: 8,
         photo: Foto8,
-        description: 'You can adjust the book categories, the amount of books displayed and corresponding pages.',
+        description: 'If you agree, you can confirm your loan, if not, click "cancel". Pay attention to the book delivery dates.',
     },
     {
         id: 9,
         photo: Foto9,
-        description:
-            'Click the arrow to read more about the book. You can rate the book, click on the star that you like the most. Click on the cover to see the enlarged photo.',
+        description: 'If you are blocked, or want to see the delivery date, go to the "My Borrow books" page. To return the book, click on "return book". If the book is attracted, the lines will be colored red.',
     },
     {
         id: 10,
