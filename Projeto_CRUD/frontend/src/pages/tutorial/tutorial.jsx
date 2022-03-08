42591caba1e07060607cbbc66790254fbac761f6
Wilian Krinke <krinkewilian@gmail.com>
2022-03-07 23:26:18 -0300

    refatoração

--- Projeto_CRUD/frontend/src/pages/tutorial/tutorial.jsx
+++ Projeto_CRUD/frontend/src/pages/tutorial/tutorial.jsx
@@ -1,16 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 import Tutorialcarousel from '../../components/carousel/tutorialCarousel.jsx';
 import InternalFooter from '../../components/footer/InternalFooter.jsx';
 import HeaderComponent from '../../components/header/HeaderComponent.jsx';
 import Loading from '../../components/loading/Loading.jsx';
-import Menu from '../../components/menu/Menu';
+import Menu from '../../components/menu/Menu.jsx';
 import translate from '../../i18n/translate.js';
-import { TutorialMain } from './styled.js';
+import { LinkStyled, TutorialMain } from './styled.js';
 
 const Tutorial = () => {
     const [loadingState, setloadingState] = useState(true);
     const darkMode = useSelector((state) => state.toggleDarkModeReducer.darkMode);
+    const { sector } = useParams();
+
+    const isExternalTutorial = sector === 'true';
 
     useEffect(() => {
         setloadingState(false);
@@ -22,9 +26,14 @@ const Tutorial = () => {
                 <Loading />
             ) : (
                 <>
-                    <Menu />
-                    <HeaderComponent $darkmode={darkMode} phrase={translate('Tutorial')} />
+                    {isExternalTutorial && <Menu />}
+                    <HeaderComponent $darkmode={darkMode} phrase={translate('tutorial')} />
                     <TutorialMain $darkmode={darkMode}>
+                        {!isExternalTutorial && (
+                            <LinkStyled to="/" $darkmode={darkMode}>
+                                Back Home
+                            </LinkStyled>
+                        )}
                         <Tutorialcarousel />
                     </TutorialMain>
                     <InternalFooter $darkmode={darkMode} />
