9fd849061aba6b3f85387b6fcc5959c82096bb14
Wilian Krinke <krinkewilian@gmail.com>
2022-03-10 00:06:59 -0300

    fake commit sorry

--- Projeto_CRUD/frontend/src/i18n/messages/en-US.js
+++ Projeto_CRUD/frontend/src/i18n/messages/en-US.js
@@ -137,6 +137,7 @@ export default {
         //
         //tutorial
         //
-        'tutorial': 'Tutorial',    
+        'tutorial': 'Tutorial',
+        'Fake': 'fake commit sorry',  
     }
 }
